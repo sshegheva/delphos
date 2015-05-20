0373dc2cfe25791cb80942374ef66d4f0685def5
Snejana Shegheva <sshegheva@dataxu.com>
2015-05-20 18:43:24 -0400

    initial commits

---
+++ finalist_predictor/feature_transform.py
@@ -0,0 +1,64 @@
+FEATURES = []
+
+DEPARTMENT = 'department'
+LOCATION = 'location'
+DIVERSITY = 'diversity'
+TEAM_MODE = 'team_mode'
+TEAM_SIZE = 'team_size'
+TOPIC = 'topic'
+DEMO = 'demo'
+STYLE = 'style'
+CATEGORY = 'category'
+PRESENTATION_RANK = 'rank'
+GENDER_DIVERSE = 'gender_diverse'
+TEAM_NAME = 'team_name'
+PRESENTATION_IMAGE = 'image'
+JUDGES = 'judges'
+EASTER_EGG = 'easter_egg'
+
+
+class FeatureExtractor(object):
+    def __init__(self):
+        self.df = None
+
+    def create_department_feature(self):
+        pass
+
+    def create_location_feature(self):
+        pass
+
+    def create_diversity_feature(self):
+        pass
+
+    def create_team_mode_feature(self):
+        pass
+
+    def create_team_size_feature(self):
+        pass
+
+    def create_is_demo_feature(self):
+        pass
+
+    def create_style_feature(self):
+        pass
+
+    def create_category_feature(self):
+        pass
+
+    def create_presentation_rank_feature(self):
+        pass
+
+    def create_gender_diversity_feature(self):
+        pass
+
+    def create_team_name_feature(self):
+        pass
+
+    def create_presentation_image_feature(self):
+        pass
+
+    def create_easter_egg_feature(self):
+        pass
+
+    def create_judges_feature(self):
+        pass
---
+++ finalist_predictor/prediction.py
@@ -0,0 +1,39 @@
+import logging
+from sklearn import neighbors
+from sklearn import cross_validation
+
+logging.basicConfig()
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+
+n_neighbors = 5
+
+
+data = None
+FEATURES, TARGET = data
+# training data
+T = None
+
+
+def fit_classifier():
+    clf = neighbors.KNeighborsRegressor(n_neighbors)
+    clf.fit(FEATURES, TARGET)
+    return clf
+
+
+def predict(clf, query):
+    return clf.predict(query)
+
+
+def cross_validate_error(df, clf):
+    """
+    Evaluate classification error
+    :param df: data for classification
+    :param clf: classifier
+    :return: mean score and std deviation of the score
+    """
+    scores = cross_validation.cross_val_score(clf, df[FEATURES], df[TARGET], cv=5)
+    mean_score = scores.mean()
+    std_score = scores.std()
+    LOGGER.info('Accuracy:  %0.2f (+/- %0.2f)', mean_score, std_score * 2)
+    return mean_score, std_score
